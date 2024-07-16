e2a399b2cf3818a4a0babe4e00e108c4f47c244e
mahmoudalmazoon <mahmoudalmazoon6@gmail.com>
2024-07-16 19:43:15 +0300

    unifysolution

--- src/server.ts
+++ src/server.ts
@@ -1,7 +1,8 @@
 import { app } from "./app";
+import config from "./config/config";
 import connection from "./utils/mongoose";
 
 connection();
-const server = app.listen(9999, () => {
-  console.log(`this app is listening on port 9999....`);
+const server = app.listen(config.server.port, () => {
+  console.log(`this app is listening on port ${config.server.port}....`);
 });
